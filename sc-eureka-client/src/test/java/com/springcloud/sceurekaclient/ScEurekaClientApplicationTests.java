edd36b211c6920ed79f1e1f54683ea2956196376
lhx <1937506994@qq.com>
2019-11-25 14:06:09 +0800

    创建服务提供者client

---
+++ sc-eureka-client/src/test/java/com/springcloud/sceurekaclient/ScEurekaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcloud.sceurekaclient;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScEurekaClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
