acbe85ca2a7e9703e36a523547c464b9d9f52a0f
lhx <1937506994@qq.com>
2019-11-25 14:05:05 +0800

    创建服务提供者client

---
+++ sc-eureka-client/src/main/java/com/springcloud/sceurekaclient/produce/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.springcloud.sceurekaclient.produce.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+/**
+ *  @dept 上海软件研发中心
+ *  @description 测试服务提供者
+ *  @author HaoXin.Liu
+ *  @date 2019/11/25 11:26
+ **/
+@RestController
+@RequestMapping("test")
+public class HelloController {
+    @GetMapping("/hello")
+    public String index(@RequestParam String name) {
+        return "hello "+name+"，this is first messge";
+    }
+}
